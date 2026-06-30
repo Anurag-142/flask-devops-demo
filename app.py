cc4eb936a83802b9e04f50c9318f8ef74ebc40a5
Anurag-142 <109527876+Anurag-142@users.noreply.github.com>
2026-06-30 09:01:12 +0530

    Create basic Flask application

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return """
+    <h1>🚀 Flask DevOps Demo</h1>
+    <p>Welcome to Project 2.</p>
+    <p>This application is running successfully.</p>
+    """
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
